ee3ea89baee5d32ff03f2ce68a42fba353390a36
Vabalabadabdab <70639393+Vabalabadabdab@users.noreply.github.com>
2022-02-21 19:44:37 +0200

    year added(cpp)

--- Source.cpp
+++ Source.cpp
@@ -11,8 +11,9 @@ private:
 	double width;
 	double durability;
 	string id;
+	int year;
 public:
-	Banknote(int price, string currency, double height, double width, string id) : price(price), currency(currency), height(height), width(width), durability(100), id(id) {}
+	Banknote(int price, string currency, double height, double width, string id, int year) : price(price), currency(currency), height(height), width(width), durability(100), id(id), year(year) {}
 	void lowerDurability()
 	{
 		if (this->durability > 0) this->durability -= 20;
